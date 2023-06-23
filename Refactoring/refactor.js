7cb50b7e0d34fac235cfcf2da85e6e5f5e70c972
Michael Rose <michael@rosenetwork.net>
2023-06-23 16:40:14 -0700

    dryed refactor.js

--- Refactoring/refactor.js
+++ Refactoring/refactor.js
@@ -8,41 +8,21 @@
  * Can you refactor it by writing functions that reduce repetition?
  */
 function previewFullPrice(salesTax, shippingPrice) {
-  const shirtPrice = 30.99;
-  const sweatshirtPrice = 40.99;
-  const smallPosterPrice = 15.99;
-  const largePosterPrice = 22.99;
-  const mugPrice = 12.99;
+    const shirtPrice = 30.99;
+    const sweatshirtPrice = 40.99;
+    const smallPosterPrice = 15.99;
+    const largePosterPrice = 22.99;
+    const mugPrice = 12.99;
 
-  shirtPriceAfterTax = shirtPrice * (1 + salesTax);
-  shirtPriceAfterShipping = shirtPriceAfterTax + shippingPrice;
-  shirtPriceAfterShippingRounded = shirtPriceAfterShipping.toFixed(2);
+    const prices = [
+        shirtPrice,
+        sweatshirtPrice,
+        smallPosterPrice,
+        largePosterPrice,
+        mugPrice,
+    ];
 
-  sweatshirtPriceAfterTax = sweatshirtPrice * (1 + salesTax);
-  sweatshirtPriceAfterShipping = sweatshirtPriceAfterTax + shippingPrice;
-  sweatshirtPriceAfterShippingRounded = sweatshirtPriceAfterShipping.toFixed(2);
-
-  smallPosterPriceAfterTax = smallPosterPrice * (1 + salesTax);
-  smallPosterPriceAfterShipping = smallPosterPriceAfterTax + shippingPrice;
-  smallPosterPriceAfterShippingRounded =
-    smallPosterPriceAfterShipping.toFixed(2);
-
-  largePosterPriceAfterTax = largePosterPrice * (1 + salesTax);
-  largePosterPriceAfterShipping = largePosterPriceAfterTax + shippingPrice;
-  largePosterPriceAfterShippingRounded =
-    largePosterPriceAfterShipping.toFixed(2);
-
-  mugPriceAfterTax = mugPrice * (1 + salesTax);
-  mugPriceAfterShipping = mugPriceAfterTax + shippingPrice;
-  mugPriceAfterShippingRounded = mugPriceAfterShipping.toFixed(2);
-
-  return [
-    shirtPriceAfterShippingRounded,
-    sweatshirtPriceAfterShippingRounded,
-    smallPosterPriceAfterShippingRounded,
-    largePosterPriceAfterShippingRounded,
-    mugPriceAfterShippingRounded,
-  ];
+    return prices.map((n) => (n * (1 + salesTax) + shippingPrice).toFixed(2));
 }
 
 module.exports = previewFullPrice;
