3861902ad9386351a8fc672522e90b95b3918f6c
pkeen <pkeen7@gmail.com>
2023-06-24 16:33:36 -0600

    refactor firstClassFunctions.js with callback function

--- firstClassFunctions/firstClassFunctions.js
+++ firstClassFunctions/firstClassFunctions.js
@@ -18,44 +18,64 @@ const sidebarProducts = ["sweatpants", "shorts", "skirt", "baseball_cap"];
  */
 
 function formatProducts(carousel, grid, sidebar) {
-  const carouselProductsReformatted = carouselProducts.map((product) => {
-    // replace underscores with spaces
-    let spacedProduct = product.replace("_", " ");
+  // const carouselProductsReformatted = carouselProducts.map((product) => {
+  //   // replace underscores with spaces
+  //   let spacedProduct = product.replace("_", " ");
 
-    //capitalize each word
-    productWords = spacedProduct.split(" ");
-    capitalizedProductWords = productWords.map(
-      (word) => word.charAt(0).toUpperCase() + word.substring(1)
-    );
-    capitalizedProduct = capitalizedProductWords.join(" ");
-    return capitalizedProduct;
-  });
+  //   //capitalize each word
+  //   productWords = spacedProduct.split(" ");
+  //   capitalizedProductWords = productWords.map(
+  //     (word) => word.charAt(0).toUpperCase() + word.substring(1)
+  //   );
+  //   capitalizedProduct = capitalizedProductWords.join(" ");
+  //   return capitalizedProduct;
+  // });
 
-  const gridProductsReformatted = gridProducts.map((product) => {
-    // replace underscores with spaces
-    let spacedProduct = product.replace("_", " ");
+  // const gridProductsReformatted = gridProducts.map((product) => {
+  //   // replace underscores with spaces
+  //   let spacedProduct = product.replace("_", " ");
 
-    //capitalize each word
-    productWords = spacedProduct.split(" ");
-    capitalizedProductWords = productWords.map(
-      (word) => word.charAt(0).toUpperCase() + word.substring(1)
-    );
-    capitalizedProduct = capitalizedProductWords.join(" ");
-    return capitalizedProduct;
-  });
+  //   //capitalize each word
+  //   productWords = spacedProduct.split(" ");
+  //   capitalizedProductWords = productWords.map(
+  //     (word) => word.charAt(0).toUpperCase() + word.substring(1)
+  //   );
+  //   capitalizedProduct = capitalizedProductWords.join(" ");
+  //   return capitalizedProduct;
+  // });
 
-  const sidebarProductsReformatted = sidebarProducts.map((product) => {
-    // replace underscores with spaces
-    let spacedProduct = product.replace("_", " ");
+  // const sidebarProductsReformatted = sidebarProducts.map((product) => {
+  //   // replace underscores with spaces
+  //   let spacedProduct = product.replace("_", " ");
 
-    //capitalize each word
-    productWords = spacedProduct.split(" ");
-    capitalizedProductWords = productWords.map(
-      (word) => word.charAt(0).toUpperCase() + word.substring(1)
-    );
-    capitalizedProduct = capitalizedProductWords.join(" ");
-    return capitalizedProduct;
-  });
+  //   //capitalize each word
+  //   productWords = spacedProduct.split(" ");
+  //   capitalizedProductWords = productWords.map(
+  //     (word) => word.charAt(0).toUpperCase() + word.substring(1)
+  //   );
+  //   capitalizedProduct = capitalizedProductWords.join(" ");
+  //   return capitalizedProduct;
+  // });
+
+  const productsReformatted = (products) => {
+    return  products.map((product) => {
+      // replace underscores with spaces
+      let spacedProduct = product.replace("_", " ");
+
+      //capitalize each word
+      productWords = spacedProduct.split(" ");
+      capitalizedProductWords = productWords.map(
+        (word) => word.charAt(0).toUpperCase() + word.substring(1)
+      );
+      capitalizedProduct = capitalizedProductWords.join(" ");
+      return capitalizedProduct;
+      })
+  }
+
+  const carouselProductsReformatted = productsReformatted(carousel);
+  const gridProductsReformatted = productsReformatted(grid);
+  const sidebarProductsReformatted = productsReformatted(sidebar)
+
 
   return [
     carouselProductsReformatted,
