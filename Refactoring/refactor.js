7039120e98a86eef8bb2ea9d4c92eaf388759862
pkeen <pkeen7@gmail.com>
2023-06-24 16:30:46 -0600

    Refactor refactor.js to be more concise and readable

--- Refactoring/refactor.js
+++ Refactoring/refactor.js
@@ -8,41 +8,30 @@
  * Can you refactor it by writing functions that reduce repetition?
  */
 function previewFullPrice(salesTax, shippingPrice) {
-  const shirtPrice = 30.99;
-  const sweatshirtPrice = 40.99;
-  const smallPosterPrice = 15.99;
-  const largePosterPrice = 22.99;
-  const mugPrice = 12.99;
 
-  shirtPriceAfterTax = shirtPrice * (1 + salesTax);
-  shirtPriceAfterShipping = shirtPriceAfterTax + shippingPrice;
-  shirtPriceAfterShippingRounded = shirtPriceAfterShipping.toFixed(2);
+  // Prices refactored into object
+  const prices = {
+    shirtPrice: 30.99,
+    sweatshirtPrice: 40.99,
+    smallPosterPrice: 15.99,
+    largePosterPrice: 22.99,
+    mugPrice: 12.99
+  }
 
-  sweatshirtPriceAfterTax = sweatshirtPrice * (1 + salesTax);
-  sweatshirtPriceAfterShipping = sweatshirtPriceAfterTax + shippingPrice;
-  sweatshirtPriceAfterShippingRounded = sweatshirtPriceAfterShipping.toFixed(2);
+  const pricesAfterShippingRounded = [];
 
-  smallPosterPriceAfterTax = smallPosterPrice * (1 + salesTax);
-  smallPosterPriceAfterShipping = smallPosterPriceAfterTax + shippingPrice;
-  smallPosterPriceAfterShippingRounded =
-    smallPosterPriceAfterShipping.toFixed(2);
+  // iterate over prices and add them to pricesAfterShippingArray
+  for (let key in prices) {
+    if (prices.hasOwnProperty(key)) {
+      let priceAfterTax = prices[key] * (1 + salesTax);
+      let priceAfterShipping = priceAfterTax + shippingPrice;
+      let priceAfterShippingRounded = priceAfterShipping.toFixed(2);
+      pricesAfterShippingRounded.push(priceAfterShippingRounded);
+    }
+  }
 
-  largePosterPriceAfterTax = largePosterPrice * (1 + salesTax);
-  largePosterPriceAfterShipping = largePosterPriceAfterTax + shippingPrice;
-  largePosterPriceAfterShippingRounded =
-    largePosterPriceAfterShipping.toFixed(2);
+  return pricesAfterShippingRounded;
 
-  mugPriceAfterTax = mugPrice * (1 + salesTax);
-  mugPriceAfterShipping = mugPriceAfterTax + shippingPrice;
-  mugPriceAfterShippingRounded = mugPriceAfterShipping.toFixed(2);
-
-  return [
-    shirtPriceAfterShippingRounded,
-    sweatshirtPriceAfterShippingRounded,
-    smallPosterPriceAfterShippingRounded,
-    largePosterPriceAfterShippingRounded,
-    mugPriceAfterShippingRounded,
-  ];
 }
 
 module.exports = previewFullPrice;
