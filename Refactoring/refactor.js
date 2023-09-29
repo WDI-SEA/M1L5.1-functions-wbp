178279f2d30e5efd4a2b0780dd5c09419357d409
kfac <kfacison1998@gmail.com>
2023-09-28 21:05:23 -0500

    added afterPrice funtion, math works

--- Refactoring/refactor.js
+++ Refactoring/refactor.js
@@ -7,6 +7,19 @@
  *
  * Can you refactor it by writing functions that reduce repetition?
  */
+
+// need 3 functions
+//calculate price after tax pass 2 var
+//calculate price after shiping with new afterTax price
+//round the price of after shiping to .00
+
+function priceAfter(item, tax, shipping){
+  let total = ((item*(1+tax))+shipping).toFixed(2);
+  return total;
+}
+
+console.log(priceAfter(30.99,0.08, 4.0));
+
 function previewFullPrice(salesTax, shippingPrice) {
   const shirtPrice = 30.99;
   const sweatshirtPrice = 40.99;
@@ -18,19 +31,19 @@ function previewFullPrice(salesTax, shippingPrice) {
   shirtPriceAfterShipping = shirtPriceAfterTax + shippingPrice;
   shirtPriceAfterShippingRounded = shirtPriceAfterShipping.toFixed(2);
 
+  // shirtPriceAfterShippingRounded = priceAfter(shippingPrice,salesTax,shippingPrice);
+
   sweatshirtPriceAfterTax = sweatshirtPrice * (1 + salesTax);
   sweatshirtPriceAfterShipping = sweatshirtPriceAfterTax + shippingPrice;
   sweatshirtPriceAfterShippingRounded = sweatshirtPriceAfterShipping.toFixed(2);
 
   smallPosterPriceAfterTax = smallPosterPrice * (1 + salesTax);
   smallPosterPriceAfterShipping = smallPosterPriceAfterTax + shippingPrice;
-  smallPosterPriceAfterShippingRounded =
-    smallPosterPriceAfterShipping.toFixed(2);
+  smallPosterPriceAfterShippingRounded = smallPosterPriceAfterShipping.toFixed(2);
 
   largePosterPriceAfterTax = largePosterPrice * (1 + salesTax);
   largePosterPriceAfterShipping = largePosterPriceAfterTax + shippingPrice;
-  largePosterPriceAfterShippingRounded =
-    largePosterPriceAfterShipping.toFixed(2);
+  largePosterPriceAfterShippingRounded = largePosterPriceAfterShipping.toFixed(2);
 
   mugPriceAfterTax = mugPrice * (1 + salesTax);
   mugPriceAfterShipping = mugPriceAfterTax + shippingPrice;
