5743ebbab8d8765d61bb29d2f24eb03c39facd19
mselbekk11 <mselbekk11@gmail.com>
2023-09-29 09:16:16 -0700

    solves 1

--- Refactoring/refactor.js
+++ Refactoring/refactor.js
@@ -7,27 +7,33 @@
  *
  * Can you refactor it by writing functions that reduce repetition?
  */
+
+function calculatePriceAfterShippingRounded(itemPrice, salesTax, shippingPrice) {
+  itemPriceAfterTax = itemPrice * (1 + salesTax)
+  itemPriceAfterShipping = itemPriceAfterTax + shippingPrice
+  return itemPriceAfterShipping.toFixed(2)
+}
+
+
 function previewFullPrice(salesTax, shippingPrice) {
 
-  const object = { 
-    shirtPrice: 30.99, 
-    sweatshirtPrice: 40.99, 
-    smallPosterPrice: 15.99,
-    largePosterPrice: 22.99,
-    mugPrice: 12.99
-  };
+    const shirtPrice = 30.99
+    const sweatshirtPrice = 40.99 
+    const smallPosterPrice = 15.99
+    const largePosterPrice = 22.99
+    const mugPrice = 12.99
 
-  for (const item in object) {
-    itemPriceAfterTax = item * (1 + salesTax);
-    itemPriceAfterShipping = itemPriceAfterTax + shippingPrice;
-    itemPriceAfterShippingRounded = itemPriceAfterShipping.toFixed(2);
 
 
   return [
-    itemPriceAfterShippingRounded,
+    calculatePriceAfterShippingRounded(shirtPrice, salesTax, shippingPrice),
+    calculatePriceAfterShippingRounded(sweatshirtPrice, salesTax, shippingPrice),
+    calculatePriceAfterShippingRounded(smallPosterPrice, salesTax, shippingPrice),
+    calculatePriceAfterShippingRounded(largePosterPrice, salesTax, shippingPrice),
+    calculatePriceAfterShippingRounded(mugPrice, salesTax, shippingPrice)
   ];
 }
-}
+
 
 module.exports = previewFullPrice;
 
